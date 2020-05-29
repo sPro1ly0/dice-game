a365b7b3ee3915608434fb1a08a27ee7c43fee32
Sabrina Becker <sbecka8@gmail.com>
2020-05-29 15:21:20 -0400

    Fix spacing in between lines

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let dice2 = document.querySelector('.img2');
 dice2.getAttributeNode('src').value = `./images/dice${randomNumber2}.png`;
 
 let heading = document.querySelector('h1');
+
 (randomNumber1 > randomNumber2) ? heading.innerHTML = '🚩Player 1 Wins!' 
 : (randomNumber2 > randomNumber1) ? heading.innerHTML = 'Player 2 Wins! 🚩' 
 : heading.innerHTML = 'Draw!';
